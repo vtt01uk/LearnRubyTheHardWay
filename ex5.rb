a70b6648e6ceab5892126928697ab744cef2ed72
vtt01uk <vtt01uk@gmail.com>
2016-05-03 15:56:33 +0100

    completed ex6

--- ex5.rb
+++ ex5.rb
@@ -1,18 +1,18 @@
-my_name = "User"
-my_age = 10 #lie
-my_height = 55 #inches
-my_weight = 110 #lbs
-my_eyes = "Brown"
-my_teeth = "White"
-my_hair = "black"
+name = "User"
+age = 10 #lie
+height = 55 #inches
+weight = 110 #lbs
+eyes = "Brown"
+teeth = "White"
+hair = "black"
 
-puts "Let's talk about %s." % my_name
-puts "He's %d inches tall." % my_height
-puts "He's %d pounds heavy." % my_weight
+puts "Let's talk about %s." % name
+puts "He's %d inches tall." % height
+puts "He's %d pounds heavy." % weight
 puts "Actually that's not too heavy!"
-puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
-puts "His teeth are usually %s depending on the coffee." % my_teeth
+puts "He's got %s eyes and %s hair." % [eyes, hair]
+puts "His teeth are usually %s depending on the coffee." % teeth
 
 #this line is tricky, try to get it exactly right
-puts "If I add %d, %d, and %d I get %d." % [my_age, my_height, my_weight, my_age +my_height + my_weight]
+puts "If I add %d, %d, and %d I get %d." % [age, height, weight, age + height + weight]
  
